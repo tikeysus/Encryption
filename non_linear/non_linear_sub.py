10fb21d9a3afb3654afe2d658d4f9332513a5d6a
Tigran Tumanyan <tigrantumanyann@gmail.com>
2023-12-28 13:28:51 -0500

    Added decoder.

--- non_linear/non_linear_sub.py
+++ non_linear/non_linear_sub.py
@@ -6,10 +6,13 @@ from exceptions import UnsupportedCharacterError
 
 def non_linear_encoder(message_og, key = 'secret'):
 
-    global alphabet_dict
     alphabet_dict = {letter: '' for letter in range(26)}
 
     occured_chars = []
+
+    for character in message_og:
+        if ord(character) < 97 or ord(character) > 122:
+            raise UnsupportedCharacterError
 
     #Extract unqiue characters and check that each character qualifies.
     for character in key:
@@ -47,12 +50,53 @@ def non_linear_encoder(message_og, key = 'secret'):
 
     return ''.join(message_og) 
 
-print(non_linear_encoder('huge'))
-
 def non_linear_decoder(code, key = 'secret'):
-    
-    code = list(code)
+    alphabet_dict = {letter: '' for letter in range(26)}
+
+    occured_chars = []
+
     
     for character in code:
-        code[character] = chr()
+        if ord(character) < 97 or ord(character) > 122:
+            raise UnsupportedCharacterError
 
+    #Extract unqiue characters and check that each character qualifies.
+    for character in key:
+        if ord(character) < 97 or ord(character) > 122:
+            raise UnsupportedCharacterError
+        elif character not in occured_chars:
+            occured_chars.append(character)
+        else:
+            continue 
+    
+    key = ''.join(occured_chars)
+
+    occured_values = [] 
+
+    for unique_value in range(len(key)):
+        alphabet_dict[unique_value] = key[unique_value]
+        occured_values.append(key[unique_value]) 
+
+    alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
+
+    for i in range(len(alphabet) - 1, -1, -1):
+        if alphabet[i] in occured_values:
+            alphabet.pop(i)
+        continue 
+
+    for i in range(len(key), len(alphabet_dict)):
+        alphabet_dict[i] = alphabet[i - len(key)]
+
+    alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
+
+    code = list(code)
+
+    reversed_dict = {v: k for k, v in alphabet_dict.items()}
+
+    for i in range(len(code)):
+        code[i] = chr(reversed_dict[code[i]] + 97)
+
+    return ''.join(code) 
+    
+print(non_linear_encoder('huge', 'cmon'))
+print(non_linear_decoder('euda', '2cmon'))
