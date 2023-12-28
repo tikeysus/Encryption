d829048d9c5a48118d257fbad3ef8d940b8d937f
Tigran Tumanyan <tigrantumanyann@gmail.com>
2023-12-27 22:12:29 -0500

    please work.

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,4 @@
 class UnsupportedCharacterError(Exception):
-    def __init__(self, value = "Unsupported character"):
+    def __init__(self, value = "This text includes a character that is not supported by this algorithm. "):
         self.value = value
         super().__init__(self.value)
--- non_linear_sub.py
+++ non_linear_sub.py
@@ -7,12 +7,10 @@ from exceptions import UnsupportedCharacterError
 def non_linear_encoder(message_og, key = 'secret'):
     alphabet_dict = {letter: '' for letter in range(26)}
 
-    message = message_og.lower()
-
     occured_chars = []
 
     #Extract unqiue characters and check that each character qualifies.
-    for character in message:
+    for character in key:
         if ord(character) < 97 or ord(character) > 122:
             raise UnsupportedCharacterError
         elif character not in occured_chars:
@@ -20,13 +18,13 @@ def non_linear_encoder(message_og, key = 'secret'):
         else:
             continue 
     
-    message = ''.join(occured_chars)
+    key = ''.join(occured_chars)
 
     occured_values = [] 
 
-    for unqiue_value in range(len(message)):
-        alphabet_dict[unqiue_value] = message[unqiue_value]
-        occured_values.append(message[unqiue_value]) 
+    for unique_value in range(len(key)):
+        alphabet_dict[unique_value] = key[unique_value]
+        occured_values.append(key[unique_value]) 
 
     alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
 
@@ -35,8 +33,8 @@ def non_linear_encoder(message_og, key = 'secret'):
             alphabet.pop(i)
         continue 
 
-    for i in range(len(message), len(alphabet_dict)):
-        alphabet_dict[i] = alphabet[i - len(message)]
+    for i in range(len(key), len(alphabet_dict)):
+        alphabet_dict[i] = alphabet[i - len(key)]
 
     alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
 
@@ -46,9 +44,5 @@ def non_linear_encoder(message_og, key = 'secret'):
         message_og[i] = alphabet_dict[ord(message_og[i]) - 97] 
 
     return ''.join(message_og) 
-#Definitely wrong and needs fixing. 
  
-
-
-
-    
+print(non_linear_encoder('many', 'secret'))
