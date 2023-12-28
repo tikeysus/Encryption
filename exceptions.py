6f55ac00354736847744a97edd9bf6758c0bb2bb
Tigran Tumanyan <145818595+tikeysus@users.noreply.github.com>
2023-12-27 22:14:50 -0500

    Delete exceptions.py

--- exceptions.py
+++
@@ -1,4 +0,0 @@
-class UnsupportedCharacterError(Exception):
-    def __init__(self, value = "This text includes a character that is not supported by this algorithm. "):
-        self.value = value
-        super().__init__(self.value)
